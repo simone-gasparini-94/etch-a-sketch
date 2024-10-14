92fb7753a855e45f91cc65b30a03fe04872b1f11
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2024-10-14 23:12:00 +0200

    added input field that changed the number of squares in the grid

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const gridContainer = document.querySelector("#grid-container");
+const submitButton = document.querySelector("#submit-button");
+const inputField = document.querySelector("#input");
 
-function createGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+function createGrid(squaresNumber) {
+    gridContainer.innerHTML = "";
+
+    let squareSize = 640 / squaresNumber;
+
+    for (let i = 0; i < squaresNumber * squaresNumber; i++) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.add("square");
+
+        squareDiv.style.height = `${squareSize}px`;
+        squareDiv.style.width = `${squareSize}px`;
+        
+        let lightness = 100;
+        const darkenGrid = () => {
+            if (lightness > 0) {
+                lightness -= 10;
+                squareDiv.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
+            };
+        };
+        squareDiv.addEventListener("mouseover", darkenGrid);
         gridContainer.appendChild(squareDiv);
     };
 };
 
-function darkenGrid() {
-    
-}
+function setSquaresNumber() {
+    const squaresNumber = parseInt(inputField.value);
+    if (squaresNumber > 0 && squaresNumber < 100) {
+        return createGrid(squaresNumber);
+    } else {
+        alert("ENTER A VALID NUMBER");
+    }
+};
 
-createGrid();
+submitButton.addEventListener("click", setSquaresNumber);
+
+createGrid(16);
